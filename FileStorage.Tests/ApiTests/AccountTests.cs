f4b31f511ebda95bb160b76faf3dd7cb25eace48
AnatoliyF <anatoliyf89@gmail.com>
2022-12-09 10:46:28 +0200

    first Integration Test

---
+++ FileStorage.Tests/ApiTests/AccountTests.cs
@@ -0,0 +1,57 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Controllers;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Security.Policy;
+using Microsoft.AspNetCore.Authorization.Policy;
+
+namespace FileStorage.Tests.ApiTests
+{
+    [Collection("Sequential")]
+    public class AccountTests
+    {
+        private /*readonly*/ HttpClient _client;
+        private readonly CustomWebApplicationFactory _factory;
+        private const string RequestUri = "api/Account/";
+
+        public AccountTests()
+        {
+            _factory = new CustomWebApplicationFactory();
+        }
+
+        [Fact]
+        public async Task Can_Get_Users()
+        {
+            //Arrange
+            _client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                });
+            }).CreateClient();
+
+            //Act
+            var httpResponse = await _client.GetAsync(RequestUri);
+
+            //Assert
+            Assert.NotNull(httpResponse);
+        }
+
+       
+    }
+}
---
+++ FileStorage.Tests/ApiTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,57 @@
+﻿using DataAccessLayer.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace FileStorage.Tests.ApiTests
+{
+
+    internal class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                RemoveLibraryDbContextRegistration(services);
+
+                var serviceProvider = GetInMemoryServiceProvider();
+
+                services.AddDbContextPool<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase(Guid.Empty.ToString());
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                using (var scope = services.BuildServiceProvider().CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                    TestHelper.SeedData(context);
+                }
+            });
+        }
+
+        private static ServiceProvider GetInMemoryServiceProvider()
+        {
+            return new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+        }
+
+        private static void RemoveLibraryDbContextRegistration(IServiceCollection services)
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbContextOptions<AppDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+        }
+
+    }
+}
